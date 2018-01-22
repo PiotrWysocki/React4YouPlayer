a21a340d5f7bf894a74dae1579ae544f86432e7f
Piotr Wysocki <399piotr@wp.pl>
2018-01-22 17:53:07 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    },
+
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
